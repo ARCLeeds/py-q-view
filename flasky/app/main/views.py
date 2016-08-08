4f826466e15570a24df6e3ab74cbca610d9a1658
Jamie Roberts <ll13j7r@leeds.ac.uk>
2016-08-08 19:06:33 +0100

    Add personalised queue feature
    also add icon to tracked files

--- flasky/app/main/views.py
+++ flasky/app/main/views.py
@@ -1,6 +1,13 @@
-from flask import render_template
+from flask import render_template, g
+from flask_login import current_user
+from flask import flash, session
 from . import main
 from ..ssh import ssh
+from ..models import User
+
+@main.before_request
+def before_request():
+    g.user = current_user
 
 
 @main.route('/')
@@ -35,19 +42,86 @@ def polaris():
     return render_template('table.html', machine = "polaris", runningJobs = runningJobs,
     waitingJobs = waitingJobs)
 
-@main.route('/modules')
-def modules():
+@main.route('/personal/queues')
+def personal_queues():
+    try:
+        name = current_user.username
+    except AttributeError:
+        return render_template('empty.html', machine = "all Arc and Polaris Machines")
+
+    connection = ssh("arc2.leeds.ac.uk", "uitjr", "Jaydee3148")
+    answer = connection.sendCommand('qstat -u '+ name)
+
+    if not answer :
+        return render_template('empty.html', machine = "all Arc and Polaris Machines")
+
+    runningJobs, waitingJobs = table(answer)
+
+    return render_template('table.html', machine = "all Arc and Polaris Machines", runningJobs = runningJobs,
+    waitingJobs = waitingJobs)
+
+@main.route('/personal/queues/arc1')
+def personal_arc1():
+    try:
+        name = current_user.username
+    except AttributeError:
+        return render_template('empty.html', machine = "Arc 1")
 
     connection = ssh("arc1.leeds.ac.uk", "uitjr", "Jaydee3148")
-    answer = connection.sendCommand('module list')
+    answer = connection.sendCommand('qstat -u '+ name)
 
-    return(answer)
+    if not answer :
+        return render_template('empty.html', machine = "Arc1")
+
+    runningJobs, waitingJobs = table(answer)
+
+    return render_template('table.html', machine = "Arc1", runningJobs = runningJobs,
+    waitingJobs = waitingJobs)
+
+
+@main.route('/personal/queues/arc2')
+def personal_arc2():
+    try:
+        name = current_user.username
+    except AttributeError:
+        return render_template('empty.html', machine = "Arc2")
+
+    connection = ssh("arc2.leeds.ac.uk", "uitjr", "Jaydee3148")
+    answer = connection.sendCommand('qstat -u '+ name)
+
+    if not answer :
+        return render_template('empty.html', machine = "Arc2")
+
+    runningJobs, waitingJobs = table(answer)
+
+    return render_template('table.html', machine = "Arc2", runningJobs = runningJobs,
+    waitingJobs = waitingJobs)
+
+@main.route('/personal/queues/polaris')
+def personal_polaris():
+    try:
+        name = current_user.username
+    except AttributeError:
+        return render_template('empty.html', machine = "Polaris")
+
+    connection = ssh("polaris.leeds.ac.uk", "uitjr", "Jaydee3148")
+    answer = connection.sendCommand('qstat -u '+ name)
+
+    if not answer :
+        return render_template('empty.html', machine = "Polaris")
+
+    runningJobs, waitingJobs = table(answer)
+
+    return render_template('table.html', machine = "Polaris", runningJobs = runningJobs,
+    waitingJobs = waitingJobs)
+
+
 def table(answer):
     numberOfLines = len(answer.split("\n"))
     lines = answer.split("\n")
     runningJobs = []
     waitingJobs = []
-    for i in range (3, numberOfLines - 1 ):
+    for i in range (2, numberOfLines - 1 ):
         line = lines[i].split()
         if line[4] == 'r':
             runningJobs.append(line)
